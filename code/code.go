9807143e00cea98ba09ae190aac2ff3443e6e776
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-04-23 19:52:04 +0200

    Cascade delete schedules

--- code/code.go
+++ code/code.go
@@ -1,9 +1,12 @@
 package code
 
 import (
+	"ferrite/schedule"
+
 	"github.com/philips-software/go-hsdp-api/iron"
 )
 
 type Code struct {
 	iron.Code
+	Schedules []schedule.Schedule `gorm:"foreignKey:CodeName;references:Name;constraint:OnDelete:CASCADE"`
 }
--- schedule/schedule.go
+++ schedule/schedule.go
@@ -1,12 +1,9 @@
 package schedule
 
 import (
-	"ferrite/cluster"
-
 	"github.com/philips-software/go-hsdp-api/iron"
 )
 
 type Schedule struct {
 	iron.Schedule
-	OnCluster cluster.Cluster `gorm:"foreignKey:Cluster"`
 }
